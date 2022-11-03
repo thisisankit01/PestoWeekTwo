f9e95b9e9edf3398eed63b6be1f94f166471598c
Ankit Pandey <ankitechzz@gmail.com>
2022-11-03 13:36:36 +0530

    added some ref comments

--- Day 4/lengthOfCSS.js
+++ Day 4/lengthOfCSS.js
@@ -9,3 +9,12 @@ window.addEventListener("click",(event)=>{ // initialized click event
   </div>`; // returns font size , width and height of every element .
   result.innerHTML = str; //innerHTML is a property of every element
 });
+
+
+//percentage(%) = a length is set in ercent is based on the length of same property of parent element.
+//em- relative unit- 1em is by default equals to parent size.
+//viewport: vw and vh: 
+          //vw: 1 vw is 1% width of your entire screen.
+                 //vh: 1 vw is 1% height of your entire screen.
+//vmin: the value will be smaller at the moment vw or vh.
+//vmax: the value will be greater at the moment vw or vh.
